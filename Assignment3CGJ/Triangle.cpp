fd62605112d9b31629c7f29150c7f1fef358b807
Manuel Gusmao <manuel.de.gusmao@tecnico.ulisboa.pt>
2025-12-05 13:55:58 +0000

    Added the previous Shape class as well as a triangle sub class, added mesh as an attribute to Shape as well

---
+++ Assignment3CGJ/Triangle.cpp
@@ -0,0 +1,7 @@
+#include "Triangle.h"
+#include <mgl.hpp>
+
+Triangle::Triangle(const glm::vec4 points[3], const GLubyte index[3], glm::vec4 color):
+Shape(points, index, color){
+	createBuffers();
+}
